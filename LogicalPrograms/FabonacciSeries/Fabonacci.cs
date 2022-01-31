a7ce448338578c1793a72b2205485b5bb8706414
aamir0143 <mdaamirreza00@gmail.com>
2022-02-01 03:19:49 +0530

    [Aamir6] Add : Added Fibonacci series program

---
+++ LogicalPrograms/FabonacciSeries/Fabonacci.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms.FabonacciSeries
+{
+    public class Fabonacci
+    {
+        public static void FabonacciSeries()
+        {
+            int n1 = 0, n2 = 1, n3, i, num;
+            Console.Write("\tEnter the number of Element: ");
+            num = Convert.ToInt32(Console.ReadLine());
+            if (num <= 1)
+            {
+                Console.WriteLine(n1);
+                return;
+            }
+            Console.WriteLine("\t"+ n1);
+            Console.WriteLine("\t"+n2);
+            for (i = 2; i < num; i++)
+            {
+                n3 = n1 + n2;
+                n1 = n2;
+                n2 = n3;
+                Console.WriteLine("\t"+n3);
+            }
+            Console.WriteLine("===========================================================================");
+            Console.ReadLine();
+        }
+    }
+}
--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -1,3 +1,42 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("==========Welcome To Logical Programs==========");
-Console.ReadLine();
+using LogicalPrograms.FabonacciSeries;
+using System;
+namespace LogicalPrograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("==========Welcome To Logical Programs==========");
+            Console.WriteLine();
+            bool flag = true;
+            while (flag)
+            {
+                try
+                {
+                    Console.WriteLine("Choose any option");
+                    Console.WriteLine("\t1. Fabonacci Seriea");
+                    Console.WriteLine("\t2. Exit");
+                    Console.Write("Enter Choice: ");
+                    int choice = Convert.ToInt32(Console.ReadLine());
+                    switch (choice)
+                    {
+                        case 1:
+                            Console.WriteLine("\n\t========== 1. Fabonacci Series==========");
+                            Fabonacci.FabonacciSeries(); //Call the method
+                            break;
+                        case 2:
+                        default:
+                            flag = false;
+                            break;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception("Invalid Input");
+                }
+            }
+
+        }
+    }
+}

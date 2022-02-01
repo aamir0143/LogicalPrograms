c83749a087bb3e8bb8dba08d69b102dfc06b6d1e
aamir0143 <mdaamirreza00@gmail.com>
2022-02-01 17:47:25 +0530

    [Aamir6] Add : Added Coupon A Numbers

--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using LogicalPrograms.CouponNumbers;
 using LogicalPrograms.FibonacciSeries;
 using LogicalPrograms.PerfectNumber;
 using LogicalPrograms.PrimeNumbers;
@@ -22,7 +23,8 @@ namespace LogicalPrograms
                     Console.WriteLine("\t2. Perfect Number");
                     Console.WriteLine("\t3. Prime Numbers");
                     Console.WriteLine("\t4. Reverse Number");
-                    Console.WriteLine("\t5. Exit");
+                    Console.WriteLine("\t5. Coupon Numbers");
+                    Console.WriteLine("\t6. Exit");
                     Console.Write("Enter Choice: ");
                     int choice = Convert.ToInt32(Console.ReadLine());
                     switch (choice)
@@ -44,6 +46,12 @@ namespace LogicalPrograms
                             Reverse.ReverseNumber();
                             break;
                         case 5:
+                            Console.WriteLine("\n\t==========5. Coupon Numbers==============");
+                            Console.Write("\tHow many numbers you wants to generate: ");
+                            int N = Convert.ToInt32(Console.ReadLine());
+                            Coupon.CouponNumber(N);
+                            break;
+                        case 6:
                         default:
                             flag = false;
                             break;
